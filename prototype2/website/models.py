193ce59508ad4bd84ce6b180206d7d165b40e059
MohammedJassem <63448996+MohammedJassem@users.noreply.github.com>
2021-05-18 13:34:06 +0200

    Add files via upload

--- prototype2/website/models.py
+++ prototype2/website/models.py
@@ -1,18 +1,53 @@
-from website import db
+from website import db,login_manager
+from website import bcrypt
+from flask_login import UserMixin
 
 
-class User(db.Model):
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
-    
-class Admin(db.Model):
+    Admin_key = db.Column(db.String(length=10), nullable=True, unique=True)
+    #roles = db.relationship('Role', secondary='user_roles')
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
+class Admin(db.Model,UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
     Admin_key = db.Column(db.String(length=10), nullable=False, unique=True)
+    #roles = db.relationship('Role', secondary='user_roles')
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
     
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -23,3 +58,16 @@ class Item(db.Model):
 
     def __repr__(self):
         return f'Item {self.name}'
+
+
+"""class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))"""
--- prototype2/website/routes.py
+++ prototype2/website/routes.py
@@ -1,25 +1,36 @@
 from website import app,db
-from flask import render_template,redirect, url_for
-from website.models import Item,User,Admin
-from website.forms import RegisterForm
-key_list = ["key1","key2","key3","key4","key5","key6",]
+from flask import render_template,redirect, url_for, flash
+from website.models import Item,User,Admin#,Role,UserRoles
+from website.forms import RegisterForm,LoginForm
+key_list = ["key1","key2","key3","key4","key5","key6"]
+from flask_login import login_user,logout_user, login_required
+
+
+
+
+
+
 @app.route('/')
 @app.route('/home')
+@login_required
 def home():
 	return render_template('home.html')
 
 
 @app.route('/MapData')
+@login_required
 def Maps_data():
 	items = Item.query.all()
 	return render_template('MapsData.html', items=items)
 
 
 @app.route('/team')
+@login_required
 def team():
 	return render_template('team.html')
 
 @app.route('/about')
+@login_required
 def About():
 	return render_template('about.html')
 
@@ -29,23 +40,50 @@ def Register():
 	form = RegisterForm()
 	
 	
-	if form.validate_on_submit() and form.Admin_key.data in key_list:
-		Admin_to_create = Admin(username=form.username.data,
+	if (form.validate_on_submit()) and (form.Admin_key.data in key_list):
+		Admin_to_create = User(username=form.username.data,
 			email_address=form.email_address.data,
-			password_hash=form.password1.data,Admin_key=form.Admin_key.data)
+			password=form.password1.data,Admin_key=form.Admin_key.data)
+		#Admin_to_create.roles.append(Role(name='Admin'))
 		db.session.add(Admin_to_create)
 		db.session.commit()
 		
 		return redirect(url_for('home'))
-	elif form.validate_on_submit() :
-		User_to_create = User(username=form.username.data,email_address=form.email_address.data,password_hash=form.password1.data)
-		
+	elif form.validate_on_submit() and (form.Admin_key.data not in key_list):
+		User_to_create = User(username=form.username.data,email_address=form.email_address.data,password=form.password1.data)
+		#User_to_create.roles.append(Role(name='Admin'))
 		db.session.add(User_to_create)
 		db.session.commit()
 		return redirect(url_for('home'))
 	if form.errors != {}: #If there are not errors from the validations
 		for err_msg in form.errors.values():
-			print(f'There was an error with creating a user: {err_msg}')
+			flash(f'There was an error with creating a user: {err_msg}', category='danger')
 
 	return render_template('register.html', form=form)
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('home'))
+        else:
+            flash('Username and password are not match! Please try again', category='danger')
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for("home"))
+
 	
